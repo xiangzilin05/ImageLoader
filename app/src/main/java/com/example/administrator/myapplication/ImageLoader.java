f67b6118889e03f0b059cd5b74ba27f7f6f1f7a2
xiangzilin05 <738448954@qq.com>
2017-07-03 15:46:47 +0800

    第一个图片加载类

--- app/src/main/java/com/example/administrator/myapplication/ImageLoader.java
+++ app/src/main/java/com/example/administrator/myapplication/ImageLoader.java
@@ -5,7 +5,17 @@ package com.example.administrator.myapplication;
  */
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.LruCache;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * 图片加载类
@@ -14,5 +24,64 @@ public class ImageLoader {
     // 图片缓存
     LruCache<String, Bitmap> mImageCache;
     // 线程池，线程数量为 CPU 的数量
+    ExecutorService mExecutorService = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
 
+    public ImageLoader() {
+        initImageCache();
+    }
+
+    private void initImageCache() {
+        // 计算可使用的最大内存
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        // 取四分之一的可用内存作为缓存
+        final int cacheSize = maxMemory / 4;
+
+        mImageCache = new LruCache<String, Bitmap>(cacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
+            }
+        };
+    }
+
+    public void displayImage(final String url, final ImageView imageView){
+        imageView.setTag(url);
+        mExecutorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                Bitmap bitmap = downloadImage(url);
+                if (bitmap == null){
+                    return;
+                }
+                if (imageView.getTag().equals(url)){
+                    imageView.setImageBitmap(bitmap);
+                }
+                mImageCache.put(url, bitmap);
+            }
+        });
+    }
+
+    public Bitmap downloadImage(String imageUrl){
+        Bitmap bitmap = null;
+        try {
+            URL url = new URL(imageUrl);
+            final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            bitmap = BitmapFactory.decodeStream(connection.getInputStream());
+            connection.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
 }
+
+
+
+
+
+
+

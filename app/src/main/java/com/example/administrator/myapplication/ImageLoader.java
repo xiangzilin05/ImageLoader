25f913da948bba50d565102ef165d752826ec750
xiangzilin05 <738448954@qq.com>
2017-07-03 15:56:48 +0800

    单一职责原则

--- app/src/main/java/com/example/administrator/myapplication/ImageLoader.java
+++ app/src/main/java/com/example/administrator/myapplication/ImageLoader.java
@@ -22,31 +22,19 @@ import java.util.concurrent.Executors;
  */
 public class ImageLoader {
     // 图片缓存
-    LruCache<String, Bitmap> mImageCache;
+    ImageCache mImageCache = new ImageCache();
     // 线程池，线程数量为 CPU 的数量
     ExecutorService mExecutorService = Executors.newFixedThreadPool(
             Runtime.getRuntime().availableProcessors()
     );
 
-    public ImageLoader() {
-        initImageCache();
-    }
-
-    private void initImageCache() {
-        // 计算可使用的最大内存
-        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-        // 取四分之一的可用内存作为缓存
-        final int cacheSize = maxMemory / 4;
-
-        mImageCache = new LruCache<String, Bitmap>(cacheSize) {
-            @Override
-            protected int sizeOf(String key, Bitmap bitmap) {
-                return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
-            }
-        };
-    }
 
     public void displayImage(final String url, final ImageView imageView){
+        Bitmap bitmap = mImageCache.get(url);
+        if (bitmap != null){
+            imageView.setImageBitmap(bitmap);
+            return;
+        }
         imageView.setTag(url);
         mExecutorService.submit(new Runnable() {
             @Override
